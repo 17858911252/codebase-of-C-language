837d55f247b746a294d5d20ac3754c54b16487b7
出流 <88051699+17858911252@users.noreply.github.com>
2021-07-29 15:06:41 +0800

    Update day1.c

--- day1.c
+++ day1.c
@@ -1,11 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 int main()
 {
 	int num1 = 0;
 	int num2 = 0;
-	scanf_s("%d%d", &num1, &num2);
+	scanf("%d%d", &num1, &num2);
 	int sum = 0;
 	sum = num1 + num2;
-	printf("和=%d\n", sum);
+	printf("sum=%d\n", sum);
 	return 0;
 }
