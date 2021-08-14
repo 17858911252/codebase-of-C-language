51cf45c54c8e8b73acfc5eb703abac30b1b6031b
出流 <88051699+17858911252@users.noreply.github.com>
2021-08-14 21:58:55 +0800

    Create day6.c

---
+++ day6.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	/*
+	 int a=1;
+	 while (a <= 100)
+	 {
+		 if (a % 2 == 1)
+			 printf("%d\n", a);
+		 a++;
+	 }*/
+	/*int a = 1;
+	while (a <= 100)
+	{
+		printf("%d\n", a);
+		a += 2;
+	}*/
+	return 0;
+}
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int numble;
+	scanf("%d", &numble);
+	switch (numble)
+	{
+	case 1:
+		printf("方案1\n");
+		break;
+	case 2:
+		printf("方案2\n");
+		break;
+	}
+	return 0;
+}
