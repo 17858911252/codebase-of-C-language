d9603969bb0d1e85796c6171119c885c3131bb76
出流 <88051699+17858911252@users.noreply.github.com>
2021-08-22 14:46:29 +0800

    Create day7.c

---
+++ day7.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int i;
+	for (i = 1; i <= 10; i++)
+		printf("%d\n", i);
+	int k;
+	for (k = 0; k = 0; k++)
+		k++;//循环0次
+	return 0;
+}
