f7c4ff5e3b17264900f5417bdd78d112b835ce3d
出流 <88051699+17858911252@users.noreply.github.com>
2021-08-25 17:20:28 +0800

    Create day8.c

---
+++ day8.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int k = 0;
+	scanf("%d", &k);
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] < k)
+			left = mid + 1;
+		else if (arr[mid]>k)
+			right = mid - 1;
+		else
+		{
+			printf("要查找的k值下标为为%d\n", mid);
+			break;
+		}
+	}
+	if (left > right)
+		printf("该值在数组中不存在\n");
+	return 0;
+}
