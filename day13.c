a05b20e05e2d67d397f8ea7daa2cb8be9cda38eb
出流 <88051699+17858911252@users.noreply.github.com>
2021-09-08 15:49:24 +0800

    Create day13.c

---
+++ day13.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+void add(int* pnum)
+{
+	(*pnum)++;
+}
+int main()
+{
+	int num = 1;
+	while (num <= 10)
+	{
+		add(&num);
+	}
+	printf("%d\n", num);
+	return 0;
+}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+ 
+int main()
+{
+	printf("%d\n", strlen("abc"));
+	char arr1[20] = { 0 };
+	char arr2[20] = { "hello world" };
+	printf("%s\n", strcpy(arr1, arr2));
+	printf("%d", printf("%d", printf("%d", 43)));
+	return
