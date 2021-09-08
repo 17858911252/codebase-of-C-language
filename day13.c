c05b8782aa44f62a5b3969cc89d06bac8dd8000e
出流 <88051699+17858911252@users.noreply.github.com>
2021-09-08 16:58:42 +0800

    Update day13.c

--- day13.c
+++ day13.c
@@ -27,3 +27,38 @@ int main()
 	printf("%d", printf("%d", printf("%d", 43)));
 	return 0;
 }
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+void test(int i)
+{
+	if (i < 9)
+	{
+		test(i + 1);
+	}
+	printf("%d ", i);
+ }
+
+int main()
+{
+	test(1);
+	return 0;
+}
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+void print(unsigned int n)
+{
+	if (n > 9)
+	{
+		print(n/10);
+	}
+	printf("%d ", n % 10);
+}
+int main()
+{
+	unsigned int  num = 0;
+	scanf("%u", &num);
+	print(num);
+	return 0;
+}
