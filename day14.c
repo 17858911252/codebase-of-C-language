9dfa1ac2cbc71eb6922e88f78d849772ca861448
出流 <88051699+17858911252@users.noreply.github.com>
2021-09-14 20:15:56 +0800

    Update day14.c

--- day14.c
+++ day14.c
@@ -75,3 +75,36 @@ int main()
 	}
 	return 0;
 }
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int arr[3][4] = { {1,2},{3,4},{5,6} };
+	int i = 0;
+	int j = 0;
+	int* p = &arr[0][0];
+	for (i = 0; i < 12; i++)
+	{
+		printf("%d ", *p);
+		p++;
+	}
+	return 0;
+}
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int arr[3][4] = { {1,2},{3,4},{5,6} };
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
