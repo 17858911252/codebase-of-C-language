d94da60fb2ed745346e0b3a2c7c46d05f5b97c88
出流 <88051699+17858911252@users.noreply.github.com>
2021-09-20 21:49:35 +0800

    Update day15.c

--- day15.c
+++ day15.c
@@ -27,3 +27,33 @@ int main()
 	printf("%p\n", &arr[0]);
 	return 0;
 }
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a, b;
+	scanf("%d%d", &a, &b);
+	system("cls");
+	printf("a=%d,b=%d\n", a, b);
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	printf("a=%d,b=%d\n", a, b);
+	return 0;
+}
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d%d", &a, &b);
+	printf("a=%d,b=%d\n", a, b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("a=%d,b=%d\n", a, b);
+	return 0;
+}
