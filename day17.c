579ce8792832c5f649a034fbc9cf75a43becd9e5
出流 <88051699+17858911252@users.noreply.github.com>
2021-09-24 19:57:28 +0800

    Update day17.c

--- day17.c
+++ day17.c
@@ -19,3 +19,34 @@ int main()
     printf("猴子第一天摘了:%d个桃子。\n", num);
     return 0;
 }
+#include <stdio.h>
+
+float taxifee(int clock, int miles)
+{
+    float money;
+    if (miles <= 3)
+    {
+        money = 14;
+        printf("费用为14\n");
+    }
+    else
+    {
+        if (clock >= 23 || clock < 5)
+        {
+            money = 13 + 1 + 2.3 * (miles - 3) * 1.2;
+            printf("夜间车费为：%f\n", money);
+        }
+        else
+        {
+            money = 13 + 1 + 2.3 * (miles - 3);
+            printf("日间车费为：%f\n", money);
+        }
+    }
+
+    return money;
+}
+int main()
+{
+    printf("打的总费用：%.1f\n", taxifee(9, 12) + taxifee(1, 12));
+    return 0;
+}
