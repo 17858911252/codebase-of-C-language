e6cb07f32f54dfcb7de64636bce275fcba8a4486
出流 <88051699+17858911252@users.noreply.github.com>
2021-09-28 23:25:14 +0800

    Update day18.c

--- day18.c
+++ day18.c
@@ -46,3 +46,22 @@ int main()
 		p++;
 	}
 	return 0;
+#include<stdio.h>
+int main()
+{
+	int arr[] = { 1,2,3,4,5 };
+	int *p[5];
+	int i;
+	for (i = 0; i <= 4; i++)
+	{
+		p[i] = &arr[i];
+	}
+	int **pp;
+	pp = p;
+	for (i = 0; i <= 4; i++)
+	{
+		printf("%d,", **pp);
+		pp++;
+	}
+	return 0;
+}
