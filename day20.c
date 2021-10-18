c360d4b098853f5eb3a45e7202860967e7946d6b
出流 <88051699+17858911252@users.noreply.github.com>
2021-10-18 16:14:42 +0800

    Update day20.c

--- day20.c
+++ day20.c
@@ -20,3 +20,39 @@ int main()
 	printf("%d %d %d", a, b, c);
 	return 0;
 }
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	
+	int n = 0;
+	scanf_s("%d", &n);
+	int f1 = 0;
+	int f2 = 1;
+	int f3 = 0;
+	while (1)
+	{
+		if (n == f2)
+		{
+			printf("%d\n", 0);
+			break;
+		}
+		else if (n < f2)
+		{
+			if (abs(f1 - n) < abs(f2 - n))
+			{
+				printf("%d", abs(f1 - n));
+				break;
+			}
+			else
+			{
+				printf("%d", abs(f2 - n));
+				break;
+			}
+		}
+			f3 = f1 + f2;
+			f1 = f2;
+			f2 = f3;
+	}
+	return 0;
+}
