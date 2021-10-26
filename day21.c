6577597c008a1ab7eb512eb22431bedc6dcfa7f8
出流 <88051699+17858911252@users.noreply.github.com>
2021-10-26 23:10:32 +0800

    Update day21.c

--- day21.c
+++ day21.c
@@ -94,3 +94,22 @@ int main()
 	printf("最大公约数是%d,最小公倍数是%d", t,k);
 	return 0;
 }
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int sign = 1;
+	double pi = 0.0, n = 1.0, term = 1.0;
+	int count = 0;
+	while (fabs(term) >= 1e-8)
+	{
+		pi = pi + term;
+		n = n + 2;
+		sign = -sign;
+		term = -sign / n;
+		count++;
+	}
+	pi = pi * 4;
+	printf("pi=%10.8f  count=%d\n", pi,count);
+	return 0;
+}
